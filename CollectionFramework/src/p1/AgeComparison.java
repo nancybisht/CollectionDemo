e214b927c0e7ced59bfb253190179cd2ed6b40a9
nancybisht <nancybisht130419@gmail.com>
2020-02-11 12:27:08 +0530

    Age Criteria changed

--- CollectionFramework/src/p1/AgeComparison.java
+++ CollectionFramework/src/p1/AgeComparison.java
@@ -8,10 +8,10 @@ public class AgeComparison implements Comparator<Employee>{
 	public int compare(Employee o1, Employee o2) {
 		if(o1.empage==o2.empage)  
 		return 0;  
-		else if(o1.empage>o2.empage)  
-		return 1;  
-		else  
+		else if(o1.empage<o2.empage)  
 		return -1;  
+		else  
+		return 1;  
 		}  
 	}
 
