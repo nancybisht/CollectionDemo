102d785f2920322b5470a655a42b46d32ff0954f
nancybisht <nancybisht130419@gmail.com>
2020-02-11 14:55:02 +0530

    object changed

--- CollectionFramework/src/p1/EmployeeTest.java
+++ CollectionFramework/src/p1/EmployeeTest.java
@@ -2,13 +2,13 @@ package p1;
 import java.util.*;
 public class EmployeeTest {
 	public static void main(String[] args) {
-		ArrayList<Employee> al=new ArrayList<Employee>(); 
+		ArrayList<Employee> a=new ArrayList<Employee>(); 
 		Employee e=new Employee(101,"Devil",23);
-		al.add(e); 
-		al.add(new Employee(106,"Sujay",27));  
-		al.add(new Employee(105,"Demon",21));  
-		Collections.sort(al,new AgeComparison());
-		for(Employee emp:al){  
+		a.add(e); 
+		a.add(new Employee(106,"Sujay",27));  
+		a.add(new Employee(105,"Demon",21));  
+		Collections.sort(a,new AgeComparison());
+		for(Employee emp:a){  
 		System.out.println(emp.empno+" "+emp.ename+" "+emp.empage);  
 		}  
 		
